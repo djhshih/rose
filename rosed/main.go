e45feb7b1f0e697b24a270b23e4efe7454a75937
David J. H. Shih <djh.shih@gmail.com>
2015-09-02 20:21:23 -0400

    Completed a functional rosed.

--- rosed/main.go
+++ rosed/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,73 +20,357 @@ const (
 	CONN_TYPE    = "tcp"
 )
 
+const protocolName = "ROSE/0.1"
+
+var (
+	ilog *log.Logger
+	wlog *log.Logger
+	elog *log.Logger
+)
+
+var tables map[string]*Table
+var tablePaths map[string]string
+
 func main() {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	initTables()
+
+	//// TEST
+
+	name := "ensembl_ids_hsapiens_head"
+	loadTable(name)
+
+	table := tables[name]
+	table.Print()
+
+	stable := NewSortedTable(table, "ensembl_gene_id")
+	fmt.Println(stable.Slice("ensembl_gene_id"))
+	fmt.Println(stable.Slice("ensembl_transcript_id"))
+
+	xs := []Identifier{"ENSG00000210100", "ENSG00000210077", "ENGSG00000111111", "ENSG00000281540"}
+	fmt.Println(stable.Map(xs, "ensembl_transcript_id"))
+	fmt.Println(stable.Map(xs, "ensembl_peptide_id"))
+
+	////
+
 	// Create listener
 	ln, err := net.Listen(CONN_TYPE, CONN_ADDRESS)
 	if err != nil {
-		fmt.Println("Error creating listener:", err.Error())
+		elog.Println("Cannot create listener:", err)
 		os.Exit(1)
 	}
 	defer ln.Close()
 
 	// Listen for incoming connections
-	fmt.Println("Listening on " + CONN_ADDRESS)
+	ilog.Println("Listening on", CONN_ADDRESS)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println("Error accepting:", err.Error())
+			elog.Println("Cannot accept:", err)
 			os.Exit(1)
+		} else {
+			ilog.Println("Accepted connection from", conn.RemoteAddr())
 		}
 		go handleRequest(conn)
 	}
 }
 
+func initTables() {
+	tables = make(map[string]*Table)
+	tablePaths = make(map[string]string)
+
+	paths := []string{
+		"test.tsv",
+		"/Users/davids/data/biomart/release-81/ensembl_ids_hsapiens_head.tsv",
+		"/Users/davids/data/biomart/release-81/ensembl_ids_hsapiens.tsv",
+		"/Users/davids/data/biomart/release-81/ensembl_ids_mmusculus.tsv",
+		"/Users/davids/data/biomart/release-81/compara_homologs_hsapiens-mmusculus.tsv",
+	}
+	addTablePaths(paths)
+}
+
+func initLogs(i io.Writer, w io.Writer, e io.Writer) {
+	ilog = log.New(i, "Info: ", log.Ldate|log.Ltime)
+	wlog = log.New(w, "Warning: ", log.Ldate|log.Ltime|log.Lshortfile)
+	elog = log.New(e, "Error: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func initResponse(status int) (r string) {
+	r = protocolName + " " + strconv.Itoa(status) + " "
+	switch status {
+	case 200:
+		r += "OK"
+	case 201:
+		r += "Created"
+	case 202:
+		r += "Accepted"
+	case 204:
+		r += "No Content"
+	case 205:
+		r += "Reset Content"
+	case 400:
+		r += "Bad Request"
+	case 401:
+		r += "Unauthorized"
+	case 404:
+		r += "Not Found"
+	case 408:
+		r += "Request Timeout"
+	case 500:
+		r += "Internal Error"
+	default:
+		r += "Unknown"
+	}
+	r += "\n"
+	if status == 200 {
+		r += "\n"
+	}
+	return
+}
+
 func handleRequest(conn net.Conn) {
+	var command string
+	var inputIds []Identifier
+
+	response := initResponse(500)
+	defer func() {
+		// Send response back
+		conn.Write([]byte(response))
+		conn.Close()
+		ilog.Println("Closed connection with", conn.RemoteAddr())
+	}()
+
 	// Read request
 	scanner := bufio.NewScanner(conn)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	// first line is the command
+	if !scanner.Scan() {
+		response = initResponse(404)
+		elog.Println("Cannot read request")
+		return
+	} else {
+		command = scanner.Text()
+		for scanner.Scan() {
+			inputIds = append(inputIds, Identifier(scanner.Text()))
+		}
 	}
-
-	var response string
 
 	// first line is the command
-	tokens := strings.Split(lines[0], " ")
-	command := tokens[0]
-	switch command {
-	case "convert":
-		n := len(tokens) - 1
-		if n == 4 {
-			convertIdentifier(tokens[1], tokens[2], tokens[3], tokens[4])
-		} else if n == 3 {
-			convertIdentifier(tokens[1], tokens[2], tokens[1], tokens[3])
+	tokens := strings.Split(command, " ")
+	n := len(tokens) - 1
+	action := tokens[0]
+
+	switch action {
+	case "map":
+		if n == 3 {
+			outputIds, err := mapIdentifiers(inputIds, tokens[1], tokens[2], tokens[3])
+
+			if err != nil {
+				response = initResponse(404)
+				elog.Println("Cannot map identifiers from", conn.RemoteAddr())
+			} else {
+				// Concatenate identifiers
+				if len(outputIds) > 0 {
+					response = initResponse(200)
+					for _, id := range outputIds {
+						response += string(id) + entryDelimiter
+					}
+				} else {
+					response = initResponse(204)
+				}
+				ilog.Println("Mapped identifiers from", conn.RemoteAddr())
+			}
 		} else {
-			response = "Error, expecting: convert <srcSpecies> <srcId> <destSpecies> <destId>\n"
+			response = initResponse(400)
+			elog.Println("Invalid command, expecting: map <srcId> <destId> <table>")
+		}
+	case "load":
+		if n == 0 {
+			if err := loadAllTables(); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(201)
+				ilog.Println("All known tables are preloaded")
+			}
+		} else if n == 1 {
+			if err := loadTable(tokens[1]); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(201)
+				ilog.Println("Table", tokens[1], "is preloaded")
+			}
+		} else {
+			response = initResponse(400)
+			elog.Println("Invalid command, expecting: load [<table> ...]")
 		}
 	case "unload":
-		unloadDatabases()
-		response = "Error: Unloaded databases.\n"
-	case "help":
-		response = "convert <srcSpecies> <srcId> <destSpecies> <destId>\n" +
-			"convert <srcSpecies> <srcId> <destId>\n" +
-			"unload\n" +
-			"help\n"
+		if n == 0 {
+			if err := unloadAllTables(); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(204)
+				ilog.Println("All extant tables are unloaded")
+			}
+		} else if n == 1 {
+			if err := unloadTable(tokens[1]); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(204)
+				ilog.Println("Table", tokens[1], "is unloaded")
+			}
+		} else {
+			response = initResponse(400)
+			elog.Println("Invalid command, expecting: unload [<table> ...]")
+		}
+	case "reload":
+		if n == 0 {
+			if err := reloadAllTables(); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(205)
+				ilog.Println("All extant tables are reloaded")
+			}
+		} else if n == 1 {
+			if err := reloadTable(tokens[1]); err != nil {
+				response = initResponse(404)
+			} else {
+				response = initResponse(205)
+				ilog.Println("Table", tokens[1], "is reloaded")
+			}
+		} else {
+			response = initResponse(400)
+			elog.Println("Invalid command, expecting: reload [<table> ...]")
+		}
+	case "list":
+		response = initResponse(200)
+		response += strings.Join(getTables(), entryDelimiter) + entryDelimiter
 	default:
-		response = "Error: Unknown command.\n"
+		response = initResponse(400)
+		elog.Println("Unknown command")
 	}
 
-	// Send response back
-	conn.Write([]byte(response))
-	conn.Close()
-
-	// log error instead of sending response back
 }
 
-func convertIdentifier(srcSpecies, srcId, destSpecies, destId string) {
-
+func mapIdentifiers(xs []Identifier, tableName, srcId, destId string) (ys []Identifier, err error) {
+	if err = loadTable(tableName); err == nil {
+		stable := NewSortedTable(tables[tableName], srcId)
+		ys = stable.Map(xs, destId)
+	}
+	return
 }
 
-func unloadDatabases() {
+func reloadTable(name string) error {
+	unloadTable(name)
+	return loadTable(name)
+}
 
+func reloadAllTables() error {
+	var err error
+	for k := range tables {
+		if e := unloadTable(k); e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func unloadTable(name string) error {
+	if _, exists := tables[name]; exists {
+		delete(tables, name)
+		return nil
+	}
+	err := errors.New("Table " + name + " does not exist")
+	wlog.Println(err)
+	return err
+}
+
+func unloadAllTables() error {
+	var err error
+	for k := range tables {
+		if e := unloadTable(k); e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func loadTable(name string) (err error) {
+	// Check if table is already loaded
+	if _, exists := tables[name]; exists {
+		// Do not reload table
+		return
+	}
+
+	// Look up path to table
+	if path, ok := tablePaths[name]; ok {
+		// Open and read table
+		f, err := os.Open(path)
+		if err != nil {
+			elog.Println(err)
+		} else {
+			defer f.Close()
+			tables[name] = NewTable(f)
+			ilog.Println("Loaded table", name)
+		}
+	} else {
+		err = errors.New("Path to table " + name + " is unknown.")
+		elog.Println(err)
+	}
+
+	return
+}
+
+func loadAllTables() error {
+	var err error
+	for k := range tablePaths {
+		if e := loadTable(k); e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func getTables() (names []string) {
+	for k := range tablePaths {
+		names = append(names, k)
+	}
+	sort.StringSlice(names).Sort()
+	return
+}
+
+func addTablePath(path string) error {
+	var err error
+	name := getTableName(path)
+	if _, exists := tablePaths[name]; exists {
+		err = errors.New("Table name conflict exists for " + name)
+		wlog.Println(err)
+	} else {
+		tablePaths[name] = path
+	}
+	return err
+}
+
+func addTablePaths(paths []string) error {
+	var err error
+	for _, p := range paths {
+		if err == nil {
+			err = addTablePath(p)
+		} else {
+			addTablePath(p)
+		}
+	}
+	return err
+}
+
+func getTableName(path string) string {
+	i := strings.LastIndex(path, "/")
+	j := strings.LastIndex(path, ".")
+	if i == -1 {
+		i = 0
+	} else {
+		i++
+	}
+	if j == -1 {
+		j = len(path)
+	}
+	return path[i:j]
 }
---
+++ rosed/table.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+)
+
+const (
+	fieldDelimiter = "\t"
+	idDelimiter    = ","
+	entryDelimiter = "\n"
+)
+
+type Identifier string
+
+type Table struct {
+	data map[string][]Identifier
+	// number of rows
+	nrows int
+	ncols int
+}
+
+func NewTable(r io.Reader) *Table {
+	t := new(Table)
+	scanner := bufio.NewScanner(r)
+	if scanner.Scan() {
+		headers := strings.Split(scanner.Text(), fieldDelimiter)
+		t.data = make(map[string][]Identifier)
+		t.ncols = len(headers)
+		m := 0
+		for scanner.Scan() {
+			tokens := strings.Split(scanner.Text(), fieldDelimiter)
+			for i, h := range headers {
+				t.data[h] = append(t.data[h], Identifier(tokens[i]))
+			}
+			m++
+		}
+		t.nrows = m
+	}
+	return t
+}
+
+func (t *Table) Print() {
+	for h, _ := range t.data {
+		fmt.Printf("%s: ", h)
+		for _, v := range t.data[h] {
+			fmt.Printf("%s ", v)
+		}
+		fmt.Println()
+	}
+}
+
+// SortedTable is sorted upon initialization and maintains the sorted order by
+// its rows index
+type SortedTable struct {
+	table *Table
+	rows  []int
+	field string
+}
+
+func NewSortedTable(t *Table, field string) *SortedTable {
+	rows := make([]int, t.nrows)
+	for i, _ := range t.data[field] {
+		rows[i] = i
+	}
+	s := &SortedTable{
+		table: t,
+		rows:  rows,
+		field: field,
+	}
+	sort.Sort(s)
+
+	return s
+}
+
+func (s *SortedTable) FieldExists(f string) bool {
+	if len(s.table.data[f]) > 0 {
+		return true
+	}
+	return false
+}
+
+func (s *SortedTable) At(i int, field string) Identifier {
+	return s.table.data[field][s.rows[i]]
+}
+
+func (s *SortedTable) Slice(field string) []Identifier {
+	unsorted := s.table.data[field]
+	sorted := make([]Identifier, len(s.rows))
+	for i, r := range s.rows {
+		sorted[i] = unsorted[r]
+	}
+	return sorted
+}
+
+// x_i must be in the field on which s has been sorted.
+func (s *SortedTable) Map(xs []Identifier, dest string) []Identifier {
+	y := make([]Identifier, len(xs))
+	if s.FieldExists(dest) {
+		srcColumn := s.Slice(s.field)
+		destColumn := s.Slice(dest)
+		for i, x := range xs {
+			j := sort.Search(
+				len(srcColumn),
+				func(k int) bool {
+					return srcColumn[k] >= x
+				},
+			)
+			// find all matches
+			var id Identifier
+			for ; j < len(srcColumn) && srcColumn[j] == x; j++ {
+				if len(destColumn[j]) > 0 {
+					if len(id) > 0 {
+						id += idDelimiter + destColumn[j]
+					} else {
+						id = destColumn[j]
+					}
+				}
+			}
+			y[i] = id
+		}
+	}
+	return y
+}
+
+func (s *SortedTable) Len() int {
+	return len(s.rows)
+}
+
+func (s *SortedTable) Swap(i, j int) {
+	s.rows[i], s.rows[j] = s.rows[j], s.rows[i]
+}
+
+func (s *SortedTable) Less(i, j int) bool {
+	return s.table.data[s.field][s.rows[i]] < s.table.data[s.field][s.rows[j]]
+}
